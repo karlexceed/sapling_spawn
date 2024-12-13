6991d9e3c4848846a7ff4bdf967ae5c25df4b339
karlexceed <alex.decker@gmail.com>
2024-12-12 22:41:07 -0600

    Update init.lua
    Refactor all "minetest" references to "core"

--- init.lua
+++ init.lua
@@ -10,37 +10,37 @@ sapling_ref = {}
 
 
 -- Load support for other mods' trees
-local modpath = minetest.get_modpath("sapling_spawn")
+local modpath = core.get_modpath("sapling_spawn")
 dofile(modpath.."/other_mods.lua")
 
 
 -- Spawn saplings
 function sapling_spawn.spawn_saplings(pos, node)
 	-- This block needs to be soil.
-	if minetest.get_item_group(minetest.get_node(pos).name, "soil") == 0 then
+	if core.get_item_group(core.get_node(pos).name, "soil") == 0 then
 		return
 	end
 	
 	-- Block above needs to be air.
 	local above = {x = pos.x, y = pos.y + 1, z = pos.z}
-	if not minetest.get_node(above).name == "air" then
+	if not core.get_node(above).name == "air" then
 		return
 	end
 	
 	-- The block cannot be underwater.
-	if minetest.get_node(above).name == "default:water_source" or minetest.get_node(above).name == "default:river_water_source" then
+	if core.get_node(above).name == "default:water_source" or core.get_node(above).name == "default:river_water_source" then
 		return
 	end
 	
 	-- Is it bright enough?
-	local light = minetest.get_node_light(above)
+	local light = core.get_node_light(above)
 	if not light or light < 13 then
 		return
 	end
 	
 	-- There must be tree nearby.
 	local proximity = 15
-	if not minetest.find_node_near(pos, proximity, {"group:tree"}) then
+	if not core.find_node_near(pos, proximity, {"group:tree"}) then
 		return
 	end
 	
@@ -49,27 +49,27 @@ function sapling_spawn.spawn_saplings(pos, node)
 	local pos1 = {x = pos.x + 9, y = pos.y + 3, z = pos.z + 9}
 	-- Observe a maximum density of trees.
 	-- In the volume defined, there are 2527 blocks. 500 is approximately 20% of that space. 100 is 3-4%.
-	if #minetest.find_nodes_in_area(pos0, pos1, {"group:tree"}) >= 100 then
+	if #core.find_nodes_in_area(pos0, pos1, {"group:tree"}) >= 100 then
 		return
 	end
 	
 	-- Observe a maximum density of saplings.
-	if #minetest.find_nodes_in_area(pos0, pos1, {"group:sapling"}) >= 1 then
+	if #core.find_nodes_in_area(pos0, pos1, {"group:sapling"}) >= 1 then
 		return
 	end
 	
 	-- Choose a meaningful sapling species.
-	local nearest_tree = minetest.find_node_near(pos, proximity, {"group:tree"})
-	local sapling_name = sapling_ref[minetest.get_node(nearest_tree).name]
+	local nearest_tree = core.find_node_near(pos, proximity, {"group:tree"})
+	local sapling_name = sapling_ref[core.get_node(nearest_tree).name]
 	
 	-- If this tree isn't in the reference table, then bail.
 	if sapling_name == nil then return end
 	
 	-- If everything worked, place a sapling.
-	minetest.set_node(above, {name = sapling_name})
+	core.set_node(above, {name = sapling_name})
 end
 
-minetest.register_abm({
+core.register_abm({
 	label = "Spawn saplings",
 	nodenames = {"group:soil"},
 	neighbors = {"group:soil"},
